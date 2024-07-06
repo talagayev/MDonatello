209d6e02832c7da6f63b155f4a7d7ec8df7f15b0
Valerij Talagayev <82884038+talagayev@users.noreply.github.com>
2024-07-07 00:46:00 +0200

    Update test_mdonatello.py
    Added tests for property classes

--- mdonatello/tests/test_mdonatello.py
+++ mdonatello/tests/test_mdonatello.py
@@ -4,17 +4,52 @@ Unit and regression test for the mdonatello package.
 
 # Import package, test suite, and other packages as needed
 import mdonatello
-from mdonatello import MoleculeVisualizer
+from mdonatello import MoleculeVisualizer, MolecularWeight, LogP, TPSA, RotatableBonds, HydrogenBondAcceptors, HydrogenBondDonors
 import pytest
 import sys
 from ipywidgets import VBox, HTML, Dropdown, Checkbox, Button
 from rdkit import Chem
-from rdkit.Chem import AllChem
+from rdkit.Chem import AllChem, Descriptors
 from MDAnalysis import Universe
 
 # Dummy data for testing
 mol = Chem.MolFromSmiles("CCO")
 AllChem.Compute2DCoords(mol)
+
+@pytest.fixture
+def molecule_benzene():
+    smiles = 'c1ccccc1'
+    return Chem.MolFromSmiles(smiles)
+
+def test_molecular_weight(molecule_benzene):
+    mw = MolecularWeight(molecule_benzene)
+    assert mw.property_value == Descriptors.MolWt(molecule_benzene)
+    assert repr(mw) == f"<p style='margin: 0; margin-left: 100px;'>Molecular Weight: <b>{Descriptors.MolWt(benzene):.2f}</b></p>"
+
+def test_logp(molecule_benzene):
+    logp = LogP(molecule_benzene)
+    assert logp.property_value == Descriptors.MolLogP(molecule_benzene)
+    assert repr(logp) == f"<p style='margin: 0; margin-left: 100px;'>LogP: <b>{Descriptors.MolLogP(molecule_benzene):.2f}</b></p>"
+
+def test_tpsa(molecule_benzene):
+    tpsa = TPSA(molecule_benzene)
+    assert tpsa.property_value == Descriptors.TPSA(molecule_benzene)
+    assert repr(tpsa) == f"<p style='margin: 0; margin-left: 100px;'>TPSA: <b>{Descriptors.TPSA(molecule_benzene):.2f}</b></p>"
+
+def test_rotatable_bonds(molecule_benzene):
+    rb = RotatableBonds(molecule_benzene)
+    assert rb.property_value == Descriptors.NumRotatableBonds(molecule_benzene)
+    assert repr(rb) == f"<p style='margin: 0; margin-left: 100px;'>Rotatable Bonds: <b>{Descriptors.NumRotatableBonds(molecule_benzene)}</b></p>"
+
+def test_hydrogen_bond_acceptors(molecule_benzene):
+    hba = HydrogenBondAcceptors(molecule_benzene)
+    assert hba.property_value == Descriptors.NumHAcceptors(molecule_benzene)
+    assert repr(hba) == f"<p style='margin: 0; margin-left: 100px;'>Hydrogen Bond Acceptors: <b>{Descriptors.NumHAcceptors(molecule_benzene)}</b></p>"
+
+def test_hydrogen_bond_donors(molecule_benzene):
+    hbd = HydrogenBondDonors(molecule_benzene)
+    assert hbd.property_value == Descriptors.NumHDonors(molecule_benzene)
+    assert repr(hbd) == f"<p style='margin: 0; margin-left: 100px;'>Hydrogen Bond Donors: <b>{Descriptors.NumHDonors(molecule_benzene)}</b></p>"
 
 def test_mdonatello_imported():
     """Sample test, will always pass so long as import statement worked"""
